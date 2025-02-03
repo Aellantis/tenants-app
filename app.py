1175965d8351b4e42928fcc8f7919b5558f71568
Marcel2 <marcel2@Marcels-MacBook-Pro.local>
2025-02-03 19:57:53 -0400

    add gunicorn

--- app.py
+++ app.py
@@ -18,19 +18,23 @@ calculations_collection = db.information
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-  pass
+    return render_template("profile.html")
+
 
 @app.route("/profile")
 def profile():
-  return render_template("profile.html")
+    return render_template("profile.html")
+
 
 @app.route("/delete/<id>", methods=["POST"])
 def delete(_id):
-  pass
+    pass
+
 
 @app.route("/something/<_id>", methods=["GET"])
 def another_func(_id):
-  pass
+    pass
+
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
